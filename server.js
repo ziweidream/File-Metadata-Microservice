aba2721137ab76a419ee2e81a7d6b5dc1c9efc8a
Glitch (dynamic-violin) <none>
2018-02-12 01:18:13 +0000

    🌚🍯 Checkpoint views/index.html:621255/2 server.js:621255/35

--- server.js
+++ server.js
@@ -28,12 +28,24 @@ app.post("/dreams", function (request, response) {
   response.sendStatus(200);
 });
 
-// Simple in-memory store for now
-var dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-];
+app.post('/', multer({ dest: './uploads/'}).single('fileToUpload'), function(req,res){
+	console.log(req.body); //form fields
+	/* example output:
+	{ title: 'abc' }
+	 */
+	res.end(req.file); //form files
+	/* example output:
+            { fieldname: 'upl',
+              originalname: 'grumpy.png',
+              encoding: '7bit',
+              mimetype: 'image/png',
+              destination: './uploads/',
+              filename: '436ec561793aa4dc475a88e84776b1b9',
+              path: 'uploads/436ec561793aa4dc475a88e84776b1b9',
+              size: 277056 }
+	 */
+	res.status(204).end();
+});
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
