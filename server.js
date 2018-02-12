d5ea8eb369bf247da8f444029af0b8fbf483de20
Glitch (dynamic-violin) <none>
2018-02-12 02:29:20 +0000

    🗃🚦 Checkpoint server.js:621255/1

--- server.js
+++ server.js
@@ -1,42 +1,17 @@
-// server.js
-// where your node app starts
 var multer = require('multer');
 var bodyParser = require('body-parser');
-
-// init project
 var express = require('express');
 var app = express();
 
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
-
-// http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 app.use(bodyParser.json());
-// http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (request, response) {
+
+app.get("/", function(request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
 
-
-
-app.post('/', multer({ dest: './uploads/'}).single('fileToUpload'), function(req,res){
-	console.log(req.body); //form fields
-	/* example output:
-	{ title: 'abc' }
-	 */
-	console.log(req.file); //form files
-	/* example output:
-            { fieldname: 'upl',
-              originalname: 'grumpy.png',
-              encoding: '7bit',
-              mimetype: 'image/png',
-              destination: './uploads/',
-              filename: '436ec561793aa4dc475a88e84776b1b9',
-              path: 'uploads/436ec561793aa4dc475a88e84776b1b9',
-              size: 277056 }
-	 */
-	res.status(204).end();
+app.post('/', multer({dest: './uploads/'}).single('fileToUpload'), function(req, res) {
+  res.end("Your file size: " + req.file.size);
 });
 
 // listen for requests :)
